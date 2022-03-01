fbdf578d5e4da3d58799cbff5d883e52d70157c0
NetworkChris <51341000+NetworkChris@users.noreply.github.com>
2022-03-01 23:44:11 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Option 120
+
+Python script to generate the hex string value for a DHCP Server Option 120 (SIP Server) when passed a fully qualified domain name.
+
+# Example Usage
+
+Run the script with the desired domain name as a command line argument
+```
+python3 option120.py www.example.org
+```
+
+Output example
+```         
+00:03:77:77:77:07:65:78:61:6d:70:6c:65:03:6f:72:67:00
+```
---
+++ option120.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+import sys
+
+# Read the domain name as a cli argument
+domainName = sys.argv[1]
+
+# Split the domain name on the . character and convert to a list of values
+domainName = domainName.split(".")
+
+# Define the start/end padding
+startEndMarker = "00"
+
+# Create an empty string for use within for loop
+dhcpValue = ""
+
+# Iterate over the domain name list
+
+for domain in domainName:
+    # Calculate length of domain and create hex string of result
+    domainLength = chr(len(domain)).encode("utf-8").hex()
+    
+    # Convert domain to hex
+    domain = domain.encode("utf-8").hex()
+
+    # Concatenate the domain length and domain hex values and add to the dhcpValue string
+    dhcpValue = dhcpValue + domainLength + domain
+
+# Add the startEndMarker to the output of for loop
+dhcpValue = startEndMarker + dhcpValue + startEndMarker
+
+# Print the final value in 2 character pairs delimited by a colon (:)
+print(":".join(dhcpValue[i:i+2] for i in range (0, len(dhcpValue), 2)))
